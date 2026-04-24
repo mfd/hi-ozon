e1409e16ccf13e1888c3529e0b0416bb8f4ea3b8
Kamil Kashaev <kkash@m2air-mfd.local>
2026-04-24 15:28:36 +0500

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import pkg from './package.json'
 
 export default defineConfig({
-  base: '/alfaskb-prelogin/',  // ← Для GitHub Pages поддиректории
+  base: pkg.homepage ? new URL(pkg.homepage).pathname : '/', // ← Для GitHub Pages поддиректории
   
   plugins: [react(), tailwindcss()],
   resolve: {
